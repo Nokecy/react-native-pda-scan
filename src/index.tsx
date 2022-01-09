e3d3bf76a79ad72aaa93e42f20e5d95ada08b9f0
nokecy <nokecy@163.com>
2022-01-09 23:09:58 +0800

    feat: 添加霍尼韦尔扫描枪支持

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
-import { NativeModules } from 'react-native';
+import { DeviceEventEmitter } from 'react-native';
+import HoneywellScanner from 'react-native-honeywell-scanner-v2';
 
-const { PdaScan } = NativeModules;
 
+const startReader = (fn: (receivedData: any) => void) => {
+    if (HoneywellScanner.isCompatible) {
+        HoneywellScanner.startReader().then((claimed: any) => {
+            console.log(claimed ? 'Barcode reader is claimed' : 'Barcode reader is busy');
 
-type PdaScanType = {
+            HoneywellScanner.onBarcodeReadSuccess((event: any) => {
+                console.log('Received data', event.data);
+                DeviceEventEmitter.emit('onScanReceive', event.data);
+            });
+        });
+    }
 
-};
+    return DeviceEventEmitter.addListener('onScanReceive', fn);
+}
 
-export default PdaScan as PdaScanType;
+const stopReader = () => {
+    if (HoneywellScanner.isCompatible) {
+        HoneywellScanner.stopReader().then(() => {
+            console.log("Freedom!!");
+            HoneywellScanner.offBarcodeReadSuccess();
+        });
+    }
+
+    return DeviceEventEmitter.removeAllListeners('onScanReceive');
+}
+
+export { startReader, stopReader }
