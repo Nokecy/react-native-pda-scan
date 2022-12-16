25a837f493533d9ef68a145e48f7d7f751c286b5
nokecy <Nokecy@163.com>
2022-12-16 18:10:05 +0800

    fix: 移除不必要组件

--- android/src/main/java/com/reactnativepdascan/PdaScanModule.java
+++ android/src/main/java/com/reactnativepdascan/PdaScanModule.java
@@ -4,52 +4,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Looper;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.keyence.autoid.sdk.SdkStatus;
-import com.keyence.autoid.sdk.scan.DecodeResult;
-import com.keyence.autoid.sdk.scan.ScanManager;
-import com.keyence.autoid.sdk.scan.scanparams.ScanParams;
-import com.keyence.autoid.sdk.scan.scanparams.scanParams.Collection;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ReactModule(name = PdaScanModule.NAME)
 public class PdaScanModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     public static final String NAME = "PdaScan";
     public static final String barcodeName = "android.scanservice.action.UPLOAD_BARCODE_DATA";
     public static final String nlscanBarcodeName = "nlscan.action.SCANNER_RESULT";
-    public static final String ldBarcodeName = "com.rfid.SCAN";
-    public static final String ldStopBarcodeName = "com.rfid.STOP_SCAN";
-    public static final String ldSCANBarcodeName = "com.rfid.SCAN_CMD";
-    private ScanManager mScanManager;
 
-    protected @Nullable
-    ScanManager.DataListener mScanDataListener;
-
-    //扫描数量
-    public int m_scanSize = 1;
-  public int m_scanLen = 0;
-  public boolean m_startScan = false;
-    public List<String> scanData = new ArrayList<String>();
     private final ReactApplicationContext mContext;
 
     public PdaScanModule(ReactApplicationContext reactContext) {
@@ -62,45 +36,6 @@ public class PdaScanModule extends ReactContextBaseJavaModule implements Lifecyc
       mContext.sendBroadcast(intent);
 
       registerBroadcastReceiver();
-
-      //创建 ScanManager 类的实例
-      mScanManager = ScanManager.createScanManager(this.getReactApplicationContext());
-
-      mScanDataListener = new ScanManager.DataListener() {
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        @Override
-        public void onDataReceived(DecodeResult decodeResult) {
-          //取得读码结果
-          DecodeResult.Result result = decodeResult.getResult();
-          //取得已读取的代码类别
-          String codeType = decodeResult.getCodeType();
-          //取得已读取的数据列表
-          List<String> datas = decodeResult.getDataList();
-          //取得已读取的数据
-          String data = decodeResult.getData();
-
-          if (result == DecodeResult.Result.SUCCESS){
-            String dataStr = String.join(" ", datas);
-            Toast.makeText(mContext, dataStr, Toast.LENGTH_SHORT).show();
-            WritableMap params = Arguments.createMap();
-            params.putString("scanCode", dataStr);
-            sendEvent(mContext, "onScanReceive", params);
-
-            //重置参数
-            setScanSize(1,0);
-          }
-        }
-      };
-
-      new Thread(new Runnable() {
-        @Override
-        public void run() {
-          Looper.prepare();
-          //创建读码事件接收监听者
-          mScanManager.addDataListener(mScanDataListener);
-          Looper.loop();
-        }
-      }).start();//启动线程
     }
 
     @Override
@@ -119,46 +54,16 @@ public class PdaScanModule extends ReactContextBaseJavaModule implements Lifecyc
 
     }
 
-    @ReactMethod
-    public void setScanSize(int scanSize,int scanLen) {
-      m_scanSize = scanSize;
-      m_scanLen = scanLen;
-
-      //定义存储代码类别的变量
-      ScanParams scanParams = new ScanParams();
-      //取得当前的设置值
-      SdkStatus status = mScanManager.getConfig(scanParams);
-      if (status == SdkStatus.SUCCESS){
-        scanParams.collection.method = Collection.Method.ACCUMULATE;
-        scanParams.collection.codeCountAccumulate = scanSize;
-        //反映设置值。
-        status = mScanManager.setConfig(scanParams);
-      }
-    }
-
     @Override
     public void onHostDestroy() {
       mContext.unregisterReceiver(mHeadsetPlugReceiver);
       mContext.unregisterReceiver(nlscanReceiver);
-      mContext.unregisterReceiver(ldcanReceiver);
-
-      //舍弃 ScanManager 类的实例
-      mScanManager.removeDataListener(mScanDataListener);
-      //舍弃 ScanManager 类的实例，释放资源
-      mScanManager.releaseScanManager();
     }
 
     private void registerBroadcastReceiver() {
       mContext.registerReceiver(mHeadsetPlugReceiver, new IntentFilter(barcodeName));
 
       mContext.registerReceiver(nlscanReceiver, new IntentFilter(nlscanBarcodeName));
-
-      mContext.registerReceiver(ldcanReceiver, new IntentFilter(ldBarcodeName));
-
-      mContext.registerReceiver(ldcanReceiver, new IntentFilter(ldStopBarcodeName));
-
-      mContext.registerReceiver(ldcanReceiver, new IntentFilter(ldSCANBarcodeName));
-
     }
 
     private final BroadcastReceiver mHeadsetPlugReceiver = new BroadcastReceiver() {
@@ -193,53 +98,6 @@ public class PdaScanModule extends ReactContextBaseJavaModule implements Lifecyc
             params.putString("scanCode", scanCode);
             sendEvent(mContext, "onScanReceive", params);
           }
-        }
-      }
-    };
-
-     //兰盾扫描广播
-    private final BroadcastReceiver ldcanReceiver = new BroadcastReceiver() {
-      @RequiresApi(api = Build.VERSION_CODES.O)
-      @Override
-      public void onReceive(Context context, Intent intent) {
-
-        if (intent.getAction().equals(ldSCANBarcodeName)){
-          m_startScan = true;
-        }
-
-        if (intent.getAction().equals(ldBarcodeName)) {
-          final String scanResult_1 = intent.getStringExtra("scannerdata");
-          String scanCode = scanResult_1;
-
-          if ((m_scanLen != 0 && m_scanLen != scanCode.length()) ||
-                (scanData.size() == m_scanSize&&m_scanSize!=1)){
-            return;
-          }
-
-          if (!scanData.contains(scanCode)){
-            scanData.add(scanCode);
-          }
-          if (scanData.size() == m_scanSize && m_startScan) {
-            Intent broadIntent = new Intent();
-            broadIntent.setAction(ldStopBarcodeName);
-            context.sendBroadcast(broadIntent);
-            m_startScan = false;
-          }
-        }
-
-        if (intent.getAction().equals(ldStopBarcodeName)) {
-          if (scanData.size() != m_scanSize) {
-            //重置参数
-            scanData.clear();
-            return;
-          }
-
-          String data = String.join(" ", scanData);
-          Toast.makeText(mContext, data, Toast.LENGTH_SHORT).show();
-
-          WritableMap params = Arguments.createMap();
-          params.putString("scanCode", data);
-          sendEvent(mContext, "onScanReceive", params);
         }
       }
     };
